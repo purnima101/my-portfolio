bad1ea0e72baf271fd2ab34b49cbefa79fd65ac9
Purnima Agarwal <37765473+purnima101@users.noreply.github.com>
2024-07-16 18:58:35 +0530

    Portfilio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,13 @@ const Hero = () => {
         <Spotlight className="-top-40 -left-10 md:-left-32 md:-top20 h-screen" fill="white"/>
         <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple"/>
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
-        {/* <Spotlight className="-top-40 -left-10 md:-left-32 md:-top20 h-screen" fill="white"/> */}
+    
       </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.3] relative flex items-center justify-center">
+
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      
+    </div>
     </div>
   );
 };
